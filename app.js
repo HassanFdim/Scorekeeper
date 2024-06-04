67dc47f93e174e50c443017b2296484bde2df94a
HassanFdim <hassanfdim033@icloud.com>
2024-06-04 22:42:12 +0100

    Add some css and reset Button

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const p1Button = document.querySelector("#p1button");
 const p2Button = document.querySelector("#p2button");
+const resetButton = document.querySelector("#reset");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
+const winningScoreSelect = document.querySelector("#playto");
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
@@ -11,6 +13,8 @@ p1Button.addEventListener("click", function () {
     p1Score += 1;
     if (p1Score === winningScore) {
       isGameOver = true;
+      p1Display.classList.add("winner");
+      p2Display.classList.add("loser");
     }
     p1Display.textContent = p1Score;
   }
@@ -20,7 +24,23 @@ p2Button.addEventListener("click", function () {
     p2Score += 1;
     if (p2Score === winningScore) {
       isGameOver = true;
+      p2Display.classList.add("winner");
+      p1Display.classList.add("loser");
     }
     p2Display.textContent = p2Score;
   }
 });
+winningScoreSelect.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  reset();
+});
+resetButton.addEventListener("click", reset);
+function reset() {
+  isGameOver = false;
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+  p1Display.classList.remove("winner", "loser");
+  p2Display.classList.remove("winner", "loser");
+}
